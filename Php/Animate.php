a93f418fc98e5897df81b3e224923b463b5ea2bb
Emilian Cole <emiliancole@gmail.com>
2021-05-21 12:49:53 +0200

    Create Animate.php

---
+++ Php/Animate.php
@@ -0,0 +1,101 @@
+<?php
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.30.2.5248.dba0a5744 modeling language!*/
+
+class Animate
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Animate Attributes
+  private $attributeName;
+  private $values;
+  private $dur;
+  private $repeatCount;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public function __construct($aAttributeName, $aValues, $aDur, $aRepeatCount)
+  {
+    $this->attributeName = $aAttributeName;
+    $this->values = $aValues;
+    $this->dur = $aDur;
+    $this->repeatCount = $aRepeatCount;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public function setAttributeName($aAttributeName)
+  {
+    $wasSet = false;
+    $this->attributeName = $aAttributeName;
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setValues($aValues)
+  {
+    $wasSet = false;
+    $this->values = $aValues;
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setDur($aDur)
+  {
+    $wasSet = false;
+    $this->dur = $aDur;
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setRepeatCount($aRepeatCount)
+  {
+    $wasSet = false;
+    $this->repeatCount = $aRepeatCount;
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function getAttributeName()
+  {
+    return $this->attributeName;
+  }
+
+  public function getValues()
+  {
+    return $this->values;
+  }
+
+  public function getDur()
+  {
+    return $this->dur;
+  }
+
+  public function getRepeatCount()
+  {
+    return $this->repeatCount;
+  }
+
+  public function equals($compareTo)
+  {
+    return $this == $compareTo;
+  }
+
+  public function delete()
+  {}
+  
+  public function draw()
+  {
+    echo "<animate attributeName='".$this->getAttributeName()."' values='".$this->getValues()."' 
+    dur='".$this->getDur()."'  repeatCount='".$this->getRepeatCount()."' />";
+  }
+
+}
+?>
